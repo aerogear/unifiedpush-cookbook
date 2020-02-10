9835cfd1ba84c0db3bab4e136e942c17f28c0e9e
Massimiliano Ziccardi <mziccard@redhat.com>
2020-02-10 16:20:43 +0100

    Using a better `serviceWorker`

--- webpush/hello-world-webpush/public/aerogear-sw.js
+++ webpush/hello-world-webpush/public/aerogear-sw.js
@@ -1,18 +1,18 @@
-self.addEventListener('push', event => {
-  const data = event.data.text();
-  const msg_chan = new MessageChannel();
-  clients.matchAll().then(clients => {
-    console.log('clients', clients);
-    clients.forEach(client => {
-      console.log('posting');
-      client.postMessage(data, [msg_chan.port2]);
-    });
-    if (clients.length === 0) {
-      self.registration.showNotification(data);
-    }
-  });
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
+self.addEventListener('push', event => {
+  const msg_chan = new MessageChannel();
+  event.waitUntil(
+    self.clients.matchAll().then(clientList => {
+      if (clientList.length === 0) {
+        return self.registration.showNotification(data);
+      } else {
+        clientList.forEach(client => {
+          client.postMessage(event.data.text(), [msg_chan.port2]);
+        });
+      }
+    })
+  );
 });
