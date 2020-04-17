44cd424c973ce3d1255865389e07bbb1fc91d6ff
Summers Pittman <secondsun@gmail.com>
2020-04-16 21:27:00 -0400

    React native wip

--- react-native/push/App.js
+++ react-native/push/App.js
@@ -7,6 +7,7 @@
  */
 
 import React from 'react';
+import RnUnifiedPush from '@aerogear/aerogear-reactnative-push';
 
 import {
   SafeAreaView,
@@ -27,6 +28,22 @@ import {
 
 const App: () => React$Node = () => {
 
+  
+  RnUnifiedPush.init(
+    { 
+      alias: 'rn' ,
+      url: 'http://10.0.2.2:9999/',
+      senderId: '294932137806',
+      variantId: 'b683ddeb-dc54-41e9-999c-68da4e95ff4b',
+      secret: '777338bf-15a2-4cc2-993c-37b33e4287e1'
+    },
+    () => {
+      console.log("Success")
+    },
+    (err) => {
+      console.log(err)
+    });
+
   return (
    
      <Text>
@@ -36,5 +53,43 @@ const App: () => React$Node = () => {
   );
 };
 
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
 
 export default App;
--- react-native/push/index.js
+++ react-native/push/index.js
@@ -2,10 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, NativeEventEmitter,NativeModules,RCTNativeAppEventEmitter, HMRClient,RCTDeviceEmitter} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 
+const eventEmitter = new NativeEventEmitter(NativeModules.RnUnifiedPush);
+this.eventListener = eventEmitter.addListener('onDefaultMessage', (event) => {
+    console.log("You have receieved a push message." + JSON.stringify(event));
+ });
 
 AppRegistry.registerComponent(appName, () => App);
