da8585408170f8035fb6fb50f30d32b34e286dd3
Summers Pittman <secondsun@gmail.com>
2020-04-22 08:35:10 -0400

    adding rn

--- react-native/push/android/app/src/main/java/com/push/MainApplication.java
+++ react-native/push/android/app/src/main/java/com/push/MainApplication.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactApplication;
 import org.aerogear.unifiedpush.android.RnUnifiedPushPackage;
 import org.aerogear.unifiedpush.android.RnUnifiedPushPackage;
 import org.aerogear.unifiedpush.android.RnUnifiedPushPackage;
+import org.aerogear.unifiedpush.android.RnUnifiedPushPackage;
+import org.aerogear.unifiedpush.android.RnUnifiedPushPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
--- react-native/push/index.js
+++ react-native/push/index.js
@@ -2,14 +2,14 @@
  * @format
  */
 
-import {AppRegistry, NativeEventEmitter,NativeModules,RCTNativeAppEventEmitter, HMRClient,RCTDeviceEmitter} from 'react-native';
+import {AppRegistry, NativeEventEmitter,NativeModules} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 
-const eventEmitter = new NativeEventEmitter(NativeModules.RnUnifiedPush);
-this.eventListener = eventEmitter.addListener('onDefaultMessage', (event) => {
-    console.log("You have receieved a push message." + JSON.stringify(event));
- });
+//const eventEmitter = new NativeEventEmitter(NativeModules.RnUnifiedPush);
+// this.eventListener = eventEmitter.addListener('onDefaultMessage', (event) => {
+//     console.log("You have receieved a push message." + JSON.stringify(event));
+//  });
 
 AppRegistry.registerComponent(appName, () => App);
